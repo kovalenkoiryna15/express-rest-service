8c77edd9de7e2f81c7a99aeea18f6e0525ee4aee
kovalenkoiryna15 <52598991+kovalenkoiryna15@users.noreply.github.com>
2021-05-15 17:48:55 +0300

    feat: add board router

---
+++ src/resources/boards/board.model.js
@@ -0,0 +1,11 @@
+const { v4: uuidv4 } = require('uuid');
+
+class Board {
+  constructor({ id = uuidv4(), title = 'TITLE', columns = [] } = {}) {
+    this.id = id;
+    this.title = title;
+    this.columns = columns;
+  }
+}
+
+module.exports = Board;
