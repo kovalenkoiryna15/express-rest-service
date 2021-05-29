0db9aae65109818e4a57ff11fce3a4deea8d67d2
kovalenkoiryna15 <52598991+kovalenkoiryna15@users.noreply.github.com>
2021-05-29 23:13:36 +0300

    feat: add eslintrs.json

--- src/resources/users/user.types.js
+++
@@ -1,7 +0,0 @@
-/**
- * @typedef {Object} User
- * @property {string} name - Indicates a User name.
- * @property {string} login - Indicates a User login.
- * @property {string} password - Indicates a User password.
- * @property {string} id - Indicates a User id.
- */
